f97e207c4b80414c3fea0e2bdbdae44153607339
SHRUSHTI <shrushtibp@gmail.com>
2026-01-20 09:59:10 +0530

    // MINI PROJECT 1: Button background change const button = document.getElementById("colorBtn");
    button.addEventListener("click", function () {
      document.body.style.backgroundColor = "lightpink";
    });
    
    // MINI PROJECT 2: Image hover message
    const image = document.getElementById("myImage");
    const text = document.getElementById("message");
    
    image.addEventListener("mouseenter", function () {
      text.innerText = "You are hovering on the image 👀";
    });
    
    image.addEventListener("mouseleave", function () {
      text.innerText = "";
    });
    
    // MINI PROJECT 3: Box animation
    const box = document.getElementById("box");
    let position = 0;
    
    setInterval(function () {
      position += 2;
      box.style.left = position + "px";
    }, 50);

---
+++ Day-06-DOM-Events/script.js
@@ -0,0 +1,27 @@
+// MINI PROJECT 1: Button background change
+const button = document.getElementById("colorBtn");
+
+button.addEventListener("click", function () {
+  document.body.style.backgroundColor = "lightpink";
+});
+
+// MINI PROJECT 2: Image hover message
+const image = document.getElementById("myImage");
+const text = document.getElementById("message");
+
+image.addEventListener("mouseenter", function () {
+  text.innerText = "You are hovering on the image 👀";
+});
+
+image.addEventListener("mouseleave", function () {
+  text.innerText = "";
+});
+
+// MINI PROJECT 3: Box animation
+const box = document.getElementById("box");
+let position = 0;
+
+setInterval(function () {
+  position += 2;
+  box.style.left = position + "px";
+}, 50);
